51a2353d77d77932c236031c99c86c8844740ef7
darien786 <darienbaltazar2@gmail.com>
2023-06-02 08:34:19 -0600

    new

--- GameHub/src/main/java/mx/uv/GameHub/GameHubApplication.java
+++ GameHub/src/main/java/mx/uv/GameHub/GameHubApplication.java
@@ -9,7 +9,12 @@ import java.util.UUID;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import mx.uv.GameHub.Entitys.Categoria;
+import mx.uv.GameHub.Entitys.Equipo;
+import mx.uv.GameHub.Repositorys.CategoriaRepository;
+import mx.uv.GameHub.Repositorys.EquipoRepository;
 
 @SpringBootApplication
 public class GameHubApplication {
@@ -18,4 +23,39 @@ public class GameHubApplication {
 		SpringApplication.run(GameHubApplication.class, args);
 	}
 	
+	@Bean
+CommandLineRunner commandLineRunner(CategoriaRepository categoriaRepository, EquipoRepository equipoRepository) {
+    Random random = new Random();
+    return args -> {
+        List.of("Computadora", "Consola").forEach(cat -> {
+            Categoria categoria = Categoria.builder().nombre(cat).build();
+            categoriaRepository.save(categoria);
+        });
+        categoriaRepository.findAll().forEach(categoria -> {
+            for (int i = 0; i < 5; i++) {
+                if (categoria.getNombre().equals("Computadora")) {
+                    Equipo equipo = Equipo.builder()
+                            .dispositivo((long) i)
+                            .nombre("NovaGamer")
+                            .marca("Dell G Series")
+                            .estado("Disponible")
+                            .condicion("En funcionamiento")
+                            .categoria(categoria)
+                            .build();
+                    equipoRepository.save(equipo);
+                } else if (categoria.getNombre().equals("Consola")) {
+                    Equipo equipo = Equipo.builder()
+                            .dispositivo((long)i+5)
+                            .nombre("Series S")
+                            .marca("Xbox")
+                            .estado("Disponible")
+                            .condicion("En funcionamiento")
+                            .categoria(categoria)
+                            .build();
+                    equipoRepository.save(equipo);
+                }
+            }
+        });
+    };
+}
 }
